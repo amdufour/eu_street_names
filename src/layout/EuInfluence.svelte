946cf1f8340de8fede42316256fdb7c8de0377d0
Anne-Marie Dufour <annema.dufour@gmail.com>
2023-03-30 11:15:52 +0200

    Add women symbol

--- src/layout/EuInfluence.svelte
+++ src/layout/EuInfluence.svelte
@@ -10,7 +10,7 @@
   console.log("data", data);
 
   // Prepare data
-  const minNumCities = 5;
+  const minNumCities = 3;
   const numForeignCities =
     "n. of foreign cities celebrating the individual with one or more streets (current country borders)";
   const maxExportsPerCountry = [];
@@ -71,6 +71,14 @@
                     background: {colorScale(name.field)};"
                   class="rect"
                 />
+                {#if name.gender === "female"}
+                  <svg viewBox="0 0 11 17">
+                    <path
+                      d="m11,5.37c0-2.96-2.47-5.37-5.5-5.37S0,2.41,0,5.37c0,2.74,2.11,5,4.83,5.32v2.41h-2.67v1.3h2.67v2.6h1.33v-2.6h2.67v-1.3h-2.67v-2.41c2.72-.32,4.83-2.58,4.83-5.32Zm-9.67,0C1.33,3.12,3.2,1.3,5.5,1.3s4.17,1.82,4.17,4.06-1.87,4.06-4.17,4.06S1.33,7.61,1.33,5.37Z"
+                      style="fill: #445165;"
+                    />
+                  </svg>
+                {/if}
               </div>
             {/each}
           </div>
@@ -99,12 +107,25 @@
   }
   .street-names {
     display: flex;
+    flex-wrap: wrap;
     margin-left: -40px;
+    margin-bottom: -27px;
+  }
+  .street-name {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
   .rect {
     display: inline-block;
+    margin: 6px 6px 4px 0;
     margin-right: 6px;
     height: 55px;
     border-radius: 1px;
+  }
+  svg {
+    width: 11px;
+    height: auto;
+    margin-left: -5.5px;
   }
 </style>
